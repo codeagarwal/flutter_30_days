96b54fec136b598cabcf5e771b6c40866cd9d2ce
m-a-g-g <agarwalmayank319@gmail.com>
2021-04-05 01:03:30 +0530

    Day 4 Completed

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,19 +1,58 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: new Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-              fontSize: 40,
-              color: Colors.blue,
-              fontWeight: FontWeight.bold,
-              fontStyle: FontStyle.italic),
-        ),
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 35,
+            ),
+            Text("Welcome to MyApp",
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.deepPurple)),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter UserName", labelText: "UserName"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Enter Passward", labelText: "Passward"),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    child: Text("LogIn"),
+                    onPressed: () {
+                      print("Hii....This is first App by Pentagon.dev");
+                    },
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.deepPurpleAccent[200]),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
