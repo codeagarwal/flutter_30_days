d0d74ebf50fef5dbaef969bbb3124ff915a05eb1
Mayank Agarwal <agarwalmayank319@gmail.com>
2023-05-18 18:27:25 +0530

    Day 22 Completed

--- lib/Pages/cart_page.dart
+++ lib/Pages/cart_page.dart
@@ -9,8 +9,65 @@ class CartPage extends StatelessWidget {
       backgroundColor: context.canvasColor,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        title: Text("Cart", style: TextStyle(color: MyTheme.darkcreamColor, fontWeight: FontWeight.w500, fontSize: 20),),
+        title: Text(
+          "Cart",
+          style: TextStyle(
+              color: MyTheme.darkcreamColor,
+              fontWeight: FontWeight.w500,
+              fontSize: 20),
+        ),
+      ),
+      body: Column(
+        children: [_CartList().p32().expand(), Divider(), _CartTotal()],
       ),
     );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+        Text("\$9999",
+            style: TextStyle(fontSize: 32, color: context.theme.accentColor)),
+        WidthBox(30),
+        ElevatedButton(
+          onPressed: () {},
+          style: ButtonStyle(
+            backgroundColor:
+                MaterialStateProperty.all(context.theme.buttonColor),
+          ),
+          child: Text(
+            "Buy",
+            style: TextStyle(color: Colors.white),
+          ),
+        ).w32(context),
+      ]),
+    );
+  }
+}
+
+
+
+class _CartList extends StatefulWidget {
+  @override
+  State<_CartList> createState() => _CartListState();
+}
+
+class _CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) => ListTile(
+              leading: Icon(Icons.done),
+              trailing: IconButton(
+                icon: Icon(Icons.remove_circle_outline),
+                onPressed: () {},
+              ),
+          title: Text("Item1"),
+            ));
   }
 }
